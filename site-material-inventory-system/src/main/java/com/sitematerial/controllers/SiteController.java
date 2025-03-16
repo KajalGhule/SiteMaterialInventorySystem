224639aeb10de243e4242f36910a5f9a381ae24f
KajalGhule <kajalghule111@gmail.com>
2025-03-16 10:28:34 +0530

    Site api done

--- site-material-inventory-system/src/main/java/com/sitematerial/controllers/SiteController.java
+++ site-material-inventory-system/src/main/java/com/sitematerial/controllers/SiteController.java
@@ -1,10 +1,55 @@
 package com.sitematerial.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.sitematerial.entities.Site;
+import com.sitematerial.services.SiteServiceImpl;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api/sites")
+@RequestMapping("/api/sites")
 public class SiteController {
 
+    private static final Logger logger = LoggerFactory.getLogger(SiteController.class);
+
+    @Autowired
+    private SiteServiceImpl siteService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Site>> getAllSites() {
+        logger.info("GET /api/sites called");
+        return ResponseEntity.ok(siteService.getAllSites());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Site> getSiteById(@PathVariable Integer id) {
+        logger.info("GET /api/sites/{} called", id);
+        Site site = siteService.getSiteById(id);
+        return (site != null) ? ResponseEntity.ok(site) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Site> createSite(@RequestBody Site site) {
+        logger.info("POST /api/sites called");
+        return ResponseEntity.ok(siteService.createSite(site));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Site> updateSite(@PathVariable Integer id, @RequestBody Site site) {
+        logger.info("PUT /api/sites/{} called", id);
+        Site updated = siteService.updateSite(id, site);
+        return (updated != null) ? ResponseEntity.ok(updated) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSite(@PathVariable Integer id) {
+        logger.info("DELETE /api/sites/{} called", id);
+        siteService.deleteSite(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- site-material-inventory-system/src/main/java/com/sitematerial/services/SiteServiceImpl.java
+++ site-material-inventory-system/src/main/java/com/sitematerial/services/SiteServiceImpl.java
@@ -1,8 +1,58 @@
 package com.sitematerial.services;
 
+import com.sitematerial.entities.Site;
+import com.sitematerial.repositories.SiteRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class SiteServiceImpl {
+import java.util.List;
 
+@Service
+public class SiteServiceImpl implements ISiteService {
+
+    private static final Logger logger = LoggerFactory.getLogger(SiteServiceImpl.class);
+
+    @Autowired
+    private SiteRepository siteRepository;
+
+    @Override
+    public List<Site> getAllSites() {
+        logger.info("Fetching all sites");
+        return siteRepository.findAll();
+    }
+
+    @Override
+    public Site getSiteById(Integer id) {
+        logger.info("Fetching site by ID: {}", id);
+        return siteRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Site createSite(Site site) {
+        logger.info("Creating new site: {}", site.getSiteName());
+        return siteRepository.save(site);
+    }
+
+    @Override
+    public Site updateSite(Integer id, Site site) {
+        logger.info("Updating site ID: {}", id);
+        Site existingSite = siteRepository.findById(id).orElse(null);
+        if (existingSite != null) {
+            existingSite.setSiteName(site.getSiteName());
+            existingSite.setLocation(site.getLocation());
+            existingSite.setStartDate(site.getStartDate());
+            existingSite.setEndDate(site.getEndDate());
+            return siteRepository.save(existingSite);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteSite(Integer id) {
+        logger.info("Deleting site ID: {}", id);
+        siteRepository.deleteById(id);
+    }
 }
