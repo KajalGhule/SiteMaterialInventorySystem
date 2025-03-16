52af9d86adbb9b69a3ed3ac06b6bd1a562f8bad3
KajalGhule <kajalghule111@gmail.com>
2025-03-16 07:47:29 +0530

    site controller added

---
+++ site-material-inventory-system/src/main/java/com/sitematerial/controllers/SiteController.java
@@ -0,0 +1,10 @@
+package com.sitematerial.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/sites")
+public class SiteController {
+
+}
