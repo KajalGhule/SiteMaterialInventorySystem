30a4f543c40cb5f76a1e65b0403bc097cddb5d6b
KajalGhule <kajalghule111@gmail.com>
2025-03-20 14:59:32 +0530

    changes in material site stock repo

---
+++ site-material-inventory-system/src/main/java/com/sitematerial/repositories/MaterialSiteStockRepository.java
@@ -0,0 +1,14 @@
+package com.sitematerial.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.sitematerial.entities.MaterialSiteStock;
+
+public interface MaterialSiteStockRepository extends JpaRepository<MaterialSiteStock, Integer> {
+
+    @Query("SELECT m FROM MaterialSiteStock m WHERE m.currentStock <= m.material.reorderLevel")
+    List<MaterialSiteStock> findByCurrentStockLessThanEqualReorderLevel();
+}
